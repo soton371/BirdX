16d2aa170f2bd269ef0b5e31671869c57d00e18c
Soton Ahmed <soton371@gmail.com>
2023-05-21 23:05:11 +0600

    remove cmnt

--- lib/screens/contacts/contact.dart
+++ lib/screens/contacts/contact.dart
@@ -27,63 +27,29 @@ class ContactScreen extends StatelessWidget {
         ),
       ),
       child: ListView.builder(
-        itemCount: 20,
-        itemBuilder:  (context, index) => Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            CupertinoListTile(
-            padding: EdgeInsets.zero,
-            leadingSize: 70,
-            leadingToTitle: 0,
-            leading: CircleAvatar(
-              backgroundImage: NetworkImage('https://i.ibb.co/xM961wP/321556657089211.webp'),
-            ),
-            title: Text('Person $index',style: TextStyle(fontWeight: FontWeight.bold)),
-            subtitle: Text('+8801512345678$index'),
-            
-          ),
-          Divider(
-            height: 0,
-            indent: 65,
-            color: CupertinoColors.systemGrey.withOpacity(.5),
-          ),
-          ],
-        )
-        ),
-      /*
-      child: ListView(
-        children: [
-          const CupertinoListTile(
-            padding: EdgeInsets.zero,
-            leadingSize: 70,
-            leadingToTitle: 0,
-            leading: CircleAvatar(
-              backgroundImage: NetworkImage('https://i.ibb.co/xM961wP/321556657089211.webp'),
-            ),
-            title: Text('John Doe',style: TextStyle(fontWeight: FontWeight.bold)),
-            subtitle: Text('Hello! How are you?'),
-            
-          ),
-          Divider(
-            height: 0,
-            indent: 60,
-            color: CupertinoColors.systemGrey.withOpacity(.5),
-          ),
-          const CupertinoListTile(
-            padding: EdgeInsets.zero,
-            leadingSize: 70,
-            leadingToTitle: 0,
-            leading: CircleAvatar(
-              backgroundImage: NetworkImage('https://i.ibb.co/xM961wP/321556657089211.webp'),
-            ),
-            title: Text('Jane Smith',style: TextStyle(fontWeight: FontWeight.bold)),
-            subtitle: Text('Flutter is awesome!'),
-            
-          ),
-          // Add more CupertinoListTiles for additional messages
-        ],
-      ),
-      */
-      );
+          itemCount: 20,
+          itemBuilder: (context, index) => Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  CupertinoListTile(
+                    padding: EdgeInsets.zero,
+                    leadingSize: 70,
+                    leadingToTitle: 0,
+                    leading: const CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          'https://i.ibb.co/xM961wP/321556657089211.webp'),
+                    ),
+                    title: Text('Person $index',
+                        style: const TextStyle(fontWeight: FontWeight.bold)),
+                    subtitle: Text('+8801512345678$index'),
+                  ),
+                  Divider(
+                    height: 0,
+                    indent: 65,
+                    color: CupertinoColors.systemGrey.withOpacity(.5),
+                  ),
+                ],
+              )),
+    );
   }
 }
