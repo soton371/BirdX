49411f75b2dda01e9ec079f326be8bdce6d7529b
Soton Ahmed <soton371@gmail.com>
2023-05-23 22:46:53 +0600

    contact list

--- lib/screens/contacts/contact.dart
+++ lib/screens/contacts/contact.dart
@@ -8,23 +8,46 @@ class ContactScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       child: NestedScrollView(
-        headerSliverBuilder: (context,scrollIs)=>[
+        headerSliverBuilder: (context, scrollIs) => [
           CupertinoSliverNavigationBar(
+            border: Border.all(color: Colors.transparent),
             largeTitle: const Text("Contacts"),
             trailing: CupertinoButton(
-              padding: EdgeInsets.zero,
-              child: const Icon(CupertinoIcons.add), 
-              onPressed: (){}
-              ),
-              leading: CupertinoButton(
-              padding: EdgeInsets.zero,
-              child: const Text("Summary"), 
-              onPressed: (){}
-              ),
+                padding: EdgeInsets.zero,
+                child: const Icon(CupertinoIcons.add),
+                onPressed: () {}),
+            leading: CupertinoButton(
+                padding: EdgeInsets.zero,
+                child: const Text("Summary"),
+                onPressed: () {}),
           ),
-        ], 
-        body: const SizedBox()
-        ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 0,horizontal: 16),
+              child: CupertinoSearchTextField(
+                placeholder: 'Search',
+                onChanged: (text) {
+                  // Perform search operation based on text input
+                },
+              ),
+            ),
+          ),
+        ],
+        body: ListView.builder(
+            itemCount: 20,
+            itemBuilder: (context, index) => Column(
+              children: [
+                CupertinoListTile(
+                  leadingSize: 60,
+                  leadingToTitle: 8,
+                  leading: const CircleAvatar(backgroundImage: NetworkImage('https://cdn141.picsart.com/321556657089211.png'),radius: 23,),
+                  title: Text('Data :: #$index',style: const TextStyle(fontWeight: FontWeight.w600),),
+                  subtitle: Text('01234567890$index'),
+                  ),
+                  Divider(indent: 80,color: CupertinoColors.systemGrey.withOpacity(0.5),height: 5,)
+              ],
+            )),
+      ),
     );
   }
 }
