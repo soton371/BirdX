f9b9ff51b585994543b593992ed9cb32469a0735
Soton Ahmed <soton371@gmail.com>
2023-06-14 22:49:11 +0600

    date time ui

--- lib/screens/message/message_scr.dart
+++ lib/screens/message/message_scr.dart
@@ -2,6 +2,7 @@ import 'package:birdx/configs/my_sizes.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:forked_slider_button/forked_slider_button.dart';
+import 'package:intl/intl.dart';
 
 class MessageScreen extends StatefulWidget {
   const MessageScreen({super.key, this.name});
@@ -13,6 +14,11 @@ class MessageScreen extends StatefulWidget {
 
 class _MessageScreenState extends State<MessageScreen> {
   String msg = '';
+  DateTime _chosenDateTime = DateTime.now();
+  String formattedDate = '';
+  String formattedTime = '';
+  String initShowDate = DateFormat.MMMEd().format(DateTime.now());
+  String initTime = DateFormat.jm().format(DateTime.now().add(const Duration(minutes: 5)));
 
   @override
   Widget build(BuildContext context) {
@@ -33,7 +39,7 @@ class _MessageScreenState extends State<MessageScreen> {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
-                        "Mon, Jun 12",
+                        formattedDate.isEmpty ? initShowDate : formattedDate,
                         style: TextStyle(
                             fontSize: Theme.of(context)
                                 .textTheme
@@ -42,14 +48,14 @@ class _MessageScreenState extends State<MessageScreen> {
                             fontWeight: FontWeight.w600),
                       ),
                       Text(
-                        '9:30 PM',
+                        formattedTime.isEmpty ? initTime : formattedTime,
                         style: Theme.of(context).textTheme.titleLarge,
                       )
                     ],
                   ),
                   CupertinoButton(
                       padding: EdgeInsets.zero,
-                      onPressed: () {},
+                      onPressed: () => _showDatePicker(context),
                       child: const Icon(CupertinoIcons.pen))
                 ],
               ),
@@ -104,5 +110,63 @@ class _MessageScreenState extends State<MessageScreen> {
         ),
       ),
     );
+  }
+
+  //all method
+  // Show the modal that contains the CupertinoDatePicker
+  void _showDatePicker(ctx) {
+    // showCupertinoModalPopup is a built-in function of the cupertino library
+    showCupertinoModalPopup(
+        context: ctx,
+        builder: (_) => Container(
+              height: 302,
+              color: const Color.fromARGB(255, 255, 255, 255),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      // Close the modal
+                      CupertinoButton(
+                        child: const Text('Cancel'),
+                        onPressed: () => Navigator.of(ctx).pop(),
+                      ),
+
+                      //date convert
+                      CupertinoButton(
+                        child: const Text(
+                          'Done',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        onPressed: () {
+                          debugPrint("_chosenDateTime: $_chosenDateTime");
+                          formattedDate =
+                              DateFormat.MMMEd().format(_chosenDateTime);
+                          formattedTime =
+                              DateFormat.jm().format(_chosenDateTime);
+                          debugPrint("formattedDate: $formattedDate, formattedTime: $formattedTime");
+                          setState(() {
+                            
+                          });
+                          Navigator.of(ctx).pop();
+                        },
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 250,
+                    child: CupertinoDatePicker(
+                        initialDateTime: DateTime.now().add(const Duration(minutes: 5)),
+                        minimumDate: DateTime.now().add(const Duration(minutes: 4)),
+                        maximumDate: DateTime.now().add(const Duration(days: 10)),
+                        onDateTimeChanged: (val) {
+                          setState(() {
+                            _chosenDateTime = val;
+                          });
+                        }),
+                  ),
+                ],
+              ),
+            ));
   }
 }
