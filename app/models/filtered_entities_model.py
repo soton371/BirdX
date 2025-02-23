2ed28f666dd4e58fb63f61e0098e38715f3008b0
Soton Ahmed <soton371@gmail.com>
2025-02-23 20:31:03 +0600

    generation series table create

--- app/models/filtered_entities_model.py
+++ app/models/filtered_entities_model.py
@@ -25,3 +25,10 @@ class ProcessorModels(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String, nullable=False, unique=True)
 
+
+class GenerationSeriesModels(Base):
+    __tablename__ = "generation_series_models"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String, nullable=False, unique=True)
+
