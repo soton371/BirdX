f0b72ee9e71a97cf3c34bb8c704ce6fd98fe9766
Soton Ahmed <soton371@gmail.com>
2025-02-25 22:20:41 +0600

    hdd db add

--- app/models/filtered_entities_model.py
+++ app/models/filtered_entities_model.py
@@ -58,3 +58,10 @@ class RamTypes(Base):
 
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String, nullable=False, unique=True)
+
+
+class HDD(Base):
+    __tablename__ = "hdd"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String, nullable=False, unique=True)
