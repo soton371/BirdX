2a5e4f609d8d4c5aeb4627b42593cde0ae015e61
Soton Ahmed <soton371@gmail.com>
2025-03-08 12:28:37 +0600

    route for graphics

--- app/core/app_constants.py
+++ app/core/app_constants.py
@@ -1,12 +1,12 @@
 otp_mail_subject = 'One Time Password For BirdX'
 
-# for authentication
+# for authentication route
 admin_login = "/admin_login"
 send_otp = "/send_otp"
 verify_otp = "/verify_otp"
 reset_password = "/reset_password"
 
-# for filtered entities
+# for filtered entities route
 brands = "/brands"
 processor_types = "/processor_types"
 processor_models = "/processor_models"
@@ -17,4 +17,5 @@ ram_sizes = "/ram_sizes"
 ram_types = "/ram_types"
 hdd = "/hdd"
 ssd = "/ssd"
+graphics = "/graphics"
 
--- app/routers/filtered_entities_router.py
+++ app/routers/filtered_entities_router.py
@@ -587,7 +587,7 @@ async def createSSD(req: filtered_entities_schema.SSDRequest, db: Session = Depe
     
 
 @router.get(app_constants.ssd)
-async def getHDD(db: Session = Depends(get_db)):
+async def getSSD(db: Session = Depends(get_db)):
     try:
         data = filtered_entities_service.getSSDService(db=db)
         return ResponseSuccess(status_code=status.HTTP_200_OK, data=data)
@@ -627,6 +627,69 @@ async def updateSSD(id: int, req: filtered_entities_schema.SSDRequest, db: Sessi
 
     except Exception as error:
         debugPrint(f"updateSSD error: {error}")
+        return ResponseFailed()
+# ===================== End SSD =====================
+
+
+
+
+
+# ===================== Graphics =====================
+@router.post(app_constants.graphics)
+async def createGraphics(req: filtered_entities_schema.GraphicsRequest, db: Session = Depends(get_db), current_user: auth_model.Admin = Depends(oauth2.getCurrentUser)):
+    try:
+        filtered_entities_service.createGraphicsService(req=req, db=db)
+        return ResponseSuccess(status_code=status.HTTP_200_OK, message="The graphics has been successfully created.")
+
+    except HTTPException as e:
+        return ResponseFailed(status_code=e.status_code, message=e.detail)
+
+    except Exception as error:
+        debugPrint(f"createGraphics error: {error}")
+        return ResponseFailed()
+    
+
+@router.get(app_constants.graphics)
+async def getGraphics(db: Session = Depends(get_db)):
+    try:
+        data = filtered_entities_service.getGraphicsService(db=db)
+        return ResponseSuccess(status_code=status.HTTP_200_OK, data=data)
+
+    except HTTPException as e:
+        return ResponseFailed(status_code=e.status_code, message=e.detail)
+
+    except Exception as error:
+        debugPrint(f"getGraphics error: {error}")
+        return ResponseFailed()
+    
+
+
+@router.delete(f"{app_constants.graphics}"+"/{id}")
+async def deleteGraphics(id: int, db: Session = Depends(get_db), current_user: auth_model.Admin = Depends(oauth2.getCurrentUser)):
+    try:
+        filtered_entities_service.deleteGraphicsService(id=id, db=db)
+        return ResponseSuccess(status_code=status.HTTP_200_OK, message=f'The graphics has been successfully deleted.')
+
+    except HTTPException as e:
+        return ResponseFailed(status_code=e.status_code, message=e.detail)
+
+    except Exception as error:
+        debugPrint(f"deleteGraphics error: {error}")
+        return ResponseFailed()
+    
+
+
+@router.patch(f"{app_constants.graphics}"+"/{id}")
+async def updateGraphics(id: int, req: filtered_entities_schema.GraphicsRequest, db: Session = Depends(get_db), current_user: auth_model.Admin = Depends(oauth2.getCurrentUser)):
+    try:
+        filtered_entities_service.updateGraphicsService(id=id, req=req, db=db)
+        return ResponseSuccess(status_code=status.HTTP_200_OK, message=f'The graphics has been successfully updated.')
+
+    except HTTPException as e:
+        return ResponseFailed(status_code=e.status_code, message=e.detail)
+
+    except Exception as error:
+        debugPrint(f"updateGraphics error: {error}")
         return ResponseFailed()
 # ===================== End SSD =====================
 
