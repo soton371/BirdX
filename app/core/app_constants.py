c26444993cd2d954dde86157a79493d237079dd3
Soton Ahmed <soton371@gmail.com>
2025-03-08 22:25:07 +0600

    change routes

--- app/core/app_constants.py
+++ app/core/app_constants.py
@@ -1,24 +1,24 @@
 otp_mail_subject = 'One Time Password For BirdX'
 
 # for authentication route
-admin_login = "/admin_login"
-send_otp = "/send_otp"
-verify_otp = "/verify_otp"
-reset_password = "/reset_password"
+admin_login = "/admin-login"
+send_otp = "/send-otp"
+verify_otp = "/verify-otp"
+reset_password = "/reset-password"
 
 # for filtered entities route
 brands = "/brands"
-processor_types = "/processor_types"
-processor_models = "/processor_models"
-generation_series = "/generation_series"
-display_types = "/display_types"
-special_features = "/special_features"
-ram_sizes = "/ram_sizes"
-ram_types = "/ram_types"
+processor_types = "/processor-types"
+processor_models = "/processor-models"
+generation_series = "/generation-series"
+display_types = "/display-types"
+special_features = "/special-features"
+ram_sizes = "/ram-sizes"
+ram_types = "/ram-types"
 hdd = "/hdd"
 ssd = "/ssd"
 graphics = "/graphics"
-display_sizes = "/display_sizes"
+display_sizes = "/display-sizes"
 
 
 
