847e038d83c6a60e635dac3ffeee49cd534dfc39
Soton Ahmed <soton371@gmail.com>
2025-03-12 20:46:48 +0600

    empty not except

--- app/schemas/social_link_schema.py
+++ app/schemas/social_link_schema.py
@@ -1,8 +1,8 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class SocialLinkRequest(BaseModel):
-    platform: str 
-    url: str
+    platform: str = Field(min_length=3)
+    url: str = Field(min_length=3)
 
 class SocialLinkResponse(SocialLinkRequest):
     id: int
